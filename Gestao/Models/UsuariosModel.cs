8c66489e3abfb6b037b54b75b92c78911fc52d20
Jéssica Bohn <jessicabohn74@gmail.com>
2025-05-19 20:49:15 -0300

    Usuario Herda de Identity

--- Gestao/Models/UsuariosModel.cs
+++ Gestao/Models/UsuariosModel.cs
@@ -7,15 +7,5 @@ namespace Gestao.Models;
 [Table("Usuarios")]
 public class UsuariosModel : IdentityUser
 {
-    [Required(ErrorMessage = "O nome de usuário é obrigatório")]
-    [Display(Name = "Nome do Usuário")]
-    [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
-    public string UsuarioNome { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "O e-mail é obrigatório")]
-    [StringLength(50, ErrorMessage = "O tamanho máximo é 50 caracteres")]
-    [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
-    public string UsuarioEmail { get; set; } = string.Empty;
-
     public bool UsuarioStatus { get; set; }
 }
