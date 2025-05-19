811bc45b011378419dbd614d784eb8402c086db5
Jéssica Bohn <jessicabohn74@gmail.com>
2025-05-19 20:43:56 -0300

    usuario herdando de Identity

--- Gestao/Models/UsuariosModel.cs
+++ Gestao/Models/UsuariosModel.cs
@@ -1,15 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace Gestao.Models;
 
 [Table("Usuarios")]
-public class UsuariosModel
+public class UsuariosModel : IdentityUser
 {
-    [Key]
-    public Guid UsuarioId { get; set; }
-
-    [Required(ErrorMessage ="O nome de usuário é obrigatório")]
+    [Required(ErrorMessage = "O nome de usuário é obrigatório")]
     [Display(Name = "Nome do Usuário")]
     [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
     public string UsuarioNome { get; set; } = string.Empty;
@@ -19,9 +17,5 @@ public class UsuariosModel
     [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
     public string UsuarioEmail { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "A senha é obrigatória")]
-    [MinLength(8, ErrorMessage = "O tamanho mínimo da senha é 8 caracteres")]
-    [MaxLength(50, ErrorMessage = "O tamanho máximo é 50 caracteres")]
-    public string UsuarioSenha { get; set; } = string.Empty;
     public bool UsuarioStatus { get; set; }
 }
