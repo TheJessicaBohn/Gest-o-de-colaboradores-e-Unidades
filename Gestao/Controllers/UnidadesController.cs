155990987378e9fcb1da0fdc9132989f7e2da6ed
Jéssica Bohn <jessicabohn74@gmail.com>
2025-05-20 09:24:13 -0300

    Refatoração de Unidades

--- Gestao/Controllers/UnidadesController.cs
+++ Gestao/Controllers/UnidadesController.cs
@@ -1,5 +1,5 @@
-using Gestao.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Gestao.Models;
 
 namespace Gestao.Controllers;
 
@@ -12,11 +12,103 @@ public class UnidadesController : Controller
         _unidadesRepository = unidadesRepository;
     }
 
+    [HttpGet]
     public IActionResult ListarUnidades()
     {
-        var listarUnidades = _unidadesRepository.Unidades;
+        var unidades = _unidadesRepository.Unidades;
+        return View(unidades);
+    }
 
-        return View(listarUnidades);
+    public IActionResult Detalhes(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var unidade = _unidadesRepository.BuscaUnidadesPorId(id.Value);
+        if (unidade == null) return NotFound();
+
+        return View(unidade);
+    }
+
+    public IActionResult AbreViewCriar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([Bind("UnidadeId,UnidadeCodigo,UnidadeNome,EstaUnidadeAtiva")] UnidadesModel unidadesModel)
+    {
+        if (ModelState.IsValid)
+        {
+            _unidadesRepository.CriarUnidade(unidadesModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(unidadesModel);
+    }
+
+    public IActionResult AbreViewEditar(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var unidade = _unidadesRepository.BuscaUnidadesPorId(id.Value);
+        if (unidade == null) return NotFound();
+
+        return View(unidade);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult AtualizarUnidade(int id, [Bind("UnidadeId,UnidadeCodigo,UnidadeNome,EstaUnidadeAtiva")] UnidadesModel unidadesModel)
+    {
+        if (id != unidadesModel.UnidadeId) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _unidadesRepository.AtualizarUnidade(unidadesModel);
+            }
+            catch (Exception)
+            {
+                if (!ExisteUnidade(unidadesModel.UnidadeId))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(unidadesModel);
+    }
+
+    public IActionResult AbreViewDeletar(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var unidade = _unidadesRepository.BuscaUnidadesPorId(id.Value);
+        if (unidade == null) return NotFound();
+
+        return View(unidade);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Deletar(int id)
+    {
+        var unidade = _unidadesRepository.BuscaUnidadesPorId(id);
+        if (unidade != null)
+        {
+            _unidadesRepository.InativarUnidade(id);
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ExisteUnidade(int id)
+    {
+        return _unidadesRepository.Unidades.Any(u => u.UnidadeId == id);
     }
 }
-    
+
